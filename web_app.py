b647274348f3a5e5ceb023bae2ec560c74e0ce1c
Saurabh Goyal <saurabh.2561@gmail.com>
2018-01-15 23:22:15 +0530

    make host info configurable

--- web_app.py
+++ web_app.py
@@ -1,5 +1,8 @@
+import sys
+
 from web_app.app import app
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    host, port = (sys.argv[1] if len(sys.argv) > 1 else '127.0.0.1:5000').split(':')
+    app.run(debug=True, host=host, port=int(port))
